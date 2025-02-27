5b575e2056a10aeb68fe224ec4040a5f726e1493
sudowski27 <Sudowski@o2.pl>
2025-02-27 17:13:12 +0100

    Update file src/reverse_process.py

--- src/reverse_process.py
+++ src/reverse_process.py
@@ -2,13 +2,13 @@
 import torch
 
 
-def reverse_process(diffusion_model: torch.nn.Module, time: int, x_t: torch.tensor):
+def reverse_process(diffusion_model: torch.nn.Module, time: int, x_t: torch.tensor, device: str):
     """
     """
     t = time - 1
     assert t >= 0, f"t= {t} reverse_process"
 
     mu, std = diffusion_model(x_t, t)
-    epsilon = torch.randn_like(x_t)
+    epsilon = torch.randn_like(x_t, device=device)
 
     return mu, std, mu + epsilon * std
